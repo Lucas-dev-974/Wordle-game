a2604bb44c0af6c0be1299fc2eaeaa09bcfd97c8
lcs lvn <lucas.lvn97439@gmail.com>
2022-09-05 07:21:52 +0200

    base project setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import Navbar from './components/Navbar.vue';
+  import Grid from './components/Grid.vue';
+  import Keyboard from './components/Keyboard.vue';
+</script>
+
+<template>
+  <Navbar /> 
+  
+  <main class="main">
+    <div class="vertical-fit-80 flex align-vertical-center">
+      <Grid />
+    </div>
+    
+    <Keyboard />
+  </main>
+</template>
